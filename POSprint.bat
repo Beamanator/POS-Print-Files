4e9e5370f0cbfa9d65757c48ce64187f266e6db9
Beamanator <dabeamanator@gmail.com>
2018-07-03 16:55:36 +0200

    Changing commands, adding comment after printing

--- POSprint.bat
+++ POSprint.bat
@@ -1,3 +1,12 @@
-ECHO OFF
+@title POSprint Batch Window
 
+:: Turn echo on so we see what's going on
+@echo on
+
+:: Call php file to print data
 php POSprint.php
+
+@echo Batch file done
+
+:: Add 'pause' at end of file if you're hitting errors
+:: pause
